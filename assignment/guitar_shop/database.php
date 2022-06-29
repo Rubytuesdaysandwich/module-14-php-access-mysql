8e1ece1d307c8f7e838e1b20d0f0ad69ba85377f
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-06-28 19:22:41 -0600

    update

--- assignment/guitar_shop/database.php
+++ assignment/guitar_shop/database.php
@@ -1,4 +1,37 @@
 <?php
 //Create database connection
+function db_connect(){
+    $connection = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);//mysqli connection establish
+    confirm_db_connect();//confirm the connection
+    return $connection;
+}
+
+function db_disconnect($connection){
+if(isset($connection)){//if connection is set disconnect
+mysqli_close($connection);//close the connection
+}
+}
+
+function confirm_db_connect() {//confirm the connection is good
+    if(mysqli_connect_errno()) {
+      $msg = "Database connection failed: ";//if the connection fails output this $msg
+      $msg .= mysqli_connect_error();//connection error
+      $msg .= " (" . mysqli_connect_errno() . ")";
+      exit($msg);
+    }
+  }
+
+  function confirm_result_set($result_set) {
+    if (!$result_set) {
+    	exit("Database query failed.");//if database query fails then exit
+    }
+  }
+
+
+
+
+
+
+
 
 ?>
