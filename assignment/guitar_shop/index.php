e844109f7d87864d72c531dda217bdaf8c2a9c6f
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-07-01 11:35:34 -0600

    succuessfully accessing the data base with queries

--- assignment/guitar_shop/index.php
+++ assignment/guitar_shop/index.php
@@ -1,6 +1,9 @@
 <?php
 require_once('database.php');
 
+$categories = array("categoryID"=>"","categoryName"=>"");
+$products = array("productCode"=>"","productName"=>"","listPrice"=>"","productID"=>"","categoryID"=>"");
+// $category_name="";
 
 
 // Get category ID
@@ -13,21 +16,41 @@ if (!isset($category_id)) {
 }
 // Get name for selected category
 
-function find_all_subjects() {//find all subjects from subjects database
-    global $db;//grabbing db from the outside scop so it has access
+// function find_name_selected_category() {//find all subjects from subjects database
+    // global $db;//grabbing db from the outside scop so it has access
 
-    $sql = "SELECT * FROM subjects ";//select subjects//! the space after subjects is important to the query
-    $sql .= "ORDER BY position ASC";//order by position
+    $sql = "SELECT categoryName FROM categories  ";//select categories
+    $sql .= "WHERE categoryID='" . $category_id . "'";//by category name
     //*echo $sql; this can be used to trouble shoot connection
-    $result = mysqli_query($db, $sql);
-    confirm_result_set($result);//confirm the result
-    return $result;
-  }
-// Get all categories
+    $result = mysqli_query($db, $sql);//send the query to the database
+    $row = $result -> fetch_assoc();//fetch assoc array assign to $row
+    $category_name = $row['categoryName'];//get the row from the category name
 
+    // return $category_name;
+//   }
+// Get all categories,
+// function find_all_categories() {//find categories from database
+    // global $db;//grabbing db from the outside scop so it has access
+
+    $sql = "SELECT * FROM categories ";//select categories
+    $sql .= "ORDER BY categoryID  ASC";//order by position
+    //*echo $sql; this can be used to trouble shoot connection
+    $categories = mysqli_query($db, $sql);
+    // confirm_result_set($result);//confirm the result
+    // return $result;
+//   }
 
 // Get products for selected category
+//function find_products() {//find all products from my guitar database
+    //global $db;//grabbing db from the outside scop so it has access
 
+    $sql = "SELECT * FROM products ";//select subjects
+    //$sql .= "ORDER BY productID ASC";//order by position
+    //*echo $sql; this can be used to trouble shoot connection
+    $products = mysqli_query($db, $sql);
+    //confirm_result_set($result);//confirm the result
+    //return $result;
+  //}
 
 ?>
 <!DOCTYPE html>
